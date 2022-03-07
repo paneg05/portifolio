6aa9a018a7689f1bcf46075e6ce2516ac667abb0
luan <aluncard6@gmail.com>
2022-03-07 20:18:14 -0300

    atualizando layout menu lateral

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ const menuToggle = document.querySelector('.toggleMenu')
 const navigation = document.querySelector('.navigation')
 const list = document.querySelectorAll('.list')
 
+let i=0
+
+
+
+
 menuToggle.onclick=()=>{
     navigation.classList.toggle('open')
 }
@@ -9,11 +14,31 @@ menuToggle.onclick=()=>{
 function activeLink(){
     list.forEach(item => {
         item.classList.remove('active')
+        
     });
+
     this.classList.add('active')
+    
 }
+
+function scroll (){
+    let altura = window.screen.height
+    
+    window.scrollTo(0,(altura*this.i)-110)
+    
+    console.log(altura, altura*this.i)
+}
+
 
 
 list.forEach(item=>{
     item.addEventListener('click',activeLink)
+    item.addEventListener('click',scroll)
+    
+    item.i=i
+    i++
+    
 })
+
+
+
